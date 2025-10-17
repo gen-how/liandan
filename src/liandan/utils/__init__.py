c0add0d535536d4be852d5e6f4c91489e66a4a69
gen-how <billchen85@gmail.com>
2025-10-18 00:03:48 +0800

    feat: add `tensor_to_cv_mat` for visulization
    This commit introduces the `tensor_to_cv_mat` function, which
    converts a PyTorch Tensor to OpenCV Mat format. The function
    accepts a tensor of shape (C, H, W) with dtype uint8 and
    supports color format conversion between RGB and BGR.
    
    In addition, add opencv-python as a dependency.

--- src/liandan/utils/__init__.py
+++ src/liandan/utils/__init__.py
@@ -14,3 +14,25 @@ def get_default_device():
         return torch.device("cuda")
 
     return torch.device("cpu")
+
+
+def tensor_to_cv_mat(tensor: torch.Tensor, color_fmt="rgb"):
+    r"""將 PyTorch 張量轉換為 OpenCV Mat 格式。
+
+    Args:
+        tensor: 形狀為 (C, H, W) 且 dtype 為 uint8 的 3 維張量。
+        color_fmt: 輸入張量的顏色格式。可以是`"rgb"`或`"bgr"`。
+
+    Returns:
+        numpy.NDArray: 形狀為 (H, W, C) 且 dtype 為 uint8 的陣列。
+    """
+    if tensor.ndim != 3:
+        raise ValueError("Input tensor must be 3-dimensional (C, H, W).")
+    if tensor.dtype != torch.uint8:
+        raise ValueError("Input tensor must have dtype uint8.")
+
+    # If color format in Tensor is RGB, convert to BGR for OpenCV.
+    if color_fmt == "rgb":
+        tensor = tensor[[2, 1, 0], :, :]
+    # Converts from (C, H, W) to (H, W, C).
+    return tensor.permute(1, 2, 0).contiguous().numpy(force=True)
