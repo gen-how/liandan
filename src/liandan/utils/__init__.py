8f14d50d9211cf65a40145dcf73cbef6dfddf5bf
gen-how <billchen85@gmail.com>
2025-10-17 23:26:36 +0800

    docs: add docstring for `get_default_device`

--- src/liandan/utils/__init__.py
+++ src/liandan/utils/__init__.py
@@ -2,6 +2,11 @@ import torch
 
 
 def get_default_device():
+    """根據作業系統取得預設的加速計算裝置。
+
+    Returns:
+        torch.device: 代表計算裝置的物件。
+    """
     if torch.backends.mps.is_available():
         return torch.device("mps")
 
